b6f4ab6bc5e78b8529b4bf054b010bb2c4d6dc9b
Rahul kumar <rk152531@gmail.com>
2023-04-15 23:26:05 +0530

    sign in and sign out

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(express.urlencoded())
 
 app.use(cookieParser());
 const db = require('./config/mongoose');
+//use for session cookies
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+const MongoStore = require('connect-mongo');
 
 app.use(express.static('./assets'));
 
@@ -26,6 +32,31 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs')
 app.set('views', './views') 
 
+//mongo store is used to store the session cookie in the db
+
+
+
+app.use(session({
+    name : 'codial',
+    //TODO change the secret before deployment in producion mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000*60*100)
+    }, 
+    store:MongoStore.create({
+        mongoUrl:'mongodb://127.0.0.1/codeial_delopment',
+        autoRemove: 'disabled'
+    })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+
+//use express router
 app.use('/', require('./routes'));
 
 app.listen(port, function(err){
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,21 @@
 const express  = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile', usersController.profile); 
+router.get('/profile', passport.checkAuthentication, usersController.profile); 
 
 router.get('/sign-up', usersController.singUp);
 router.get('/sign-in', usersController.singIn);
 
 router.post('/create', usersController.create);
-router.post('/create-session', usersController.createSession);
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+), usersController.createSession);
+
+
+router.get('/sign-out', usersController.signOut);
 
 module.exports = router;
